ec77b815c5ac386f66c662d1a185ee1088e5c530
zbjash <110083617+zbjash@users.noreply.github.com>
2022-12-07 21:26:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@ int main(int argc, char *argv[])
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
         db.setHostName("localhost");//连接本机数据库
         db.setPort(3306);//默认端口
-        db.setDatabaseName("mydesign");//数据库名称
-        db.setUserName("root");//用户名
-        db.setPassword("20020816jiang");//用户密码
+        db.setDatabaseName("AAA");//数据库名称
+        db.setUserName("XXX");//用户名
+        db.setPassword("YYYYYYYYYY");//用户密码
         db.open();//连接数据库
 
     QApplication a(argc, argv);
-    a.addLibraryPath("./plugins");
+    a.addLibraryPath("./plugins"); //打包后能连接mysql的关键
 
 
     login w;
